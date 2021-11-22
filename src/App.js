5a57658057a260397fabb5536b4881ad7b491245
Den <zgibex333@gmail.com>
2021-11-22 16:10:42 +0300

    About Us markup + Styled components intro

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import AboutUs from "./pages/AboutUs";
+
+// Global Style
+import GlobalStyle from "./components/GlobalStyle";
+
 function App() {
-  return <div className="App">
-    <h1>Film</h1>
-  </div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <AboutUs />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/FaqSection.js
@@ -0,0 +1,31 @@
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start</h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor.</p>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor.</p>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor.</p>
+      </div>
+      <div className="question">
+        <h4>What Products do we offer</h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor.</p>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
